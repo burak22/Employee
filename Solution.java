38c5e58d151b309fb7cc2062e54823ca450f7ba7
burak22 <43789237+burak22@users.noreply.github.com>
2022-09-28 17:10:50 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,8 @@
+package Fabrika;
+
+public class Solution {
+    public static void main(String[] args) {
+        Employee e1 = new Employee("Mehmet", 5000, 45, 2020);
+        System.out.println(e1.toString());
+    }
+}
